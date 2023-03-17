114c12d09fcf09b587cf3a91934d853f2a21efa5
elainecostadev <elainerscosta@hotmail.com>
2023-03-17 19:25:45 -0300

    inclluindo styles no component Result - ajustando button para Touch - incluindo funcao de Share

--- src/components/ResultImc.js
+++ src/components/ResultImc.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Share } from 'react-native'
+import styles from '../styles/ResultImc.style'
 
 function ResultImc({ resultImc, messageResultImc}) {
+  
+  const onShare = async () => {
+    await Share.share({
+      message: `Meu IMC é ${resultImc}`,
+    })
+  }
+
   return (
-    <View>
-      <Text>{ resultImc }</Text>
-      <Text>{ messageResultImc }</Text>
+    <View style={styles.resultImc}>
+      <View style={styles.boxShareButton}>
+        { resultImc &&
+        <TouchableOpacity
+          style={styles.share}
+          onPress={onShare}
+        >
+          <Text style={styles.sharedText}>Share</Text>
+        </TouchableOpacity>
+        }
+      </View>
+      <Text style={styles.information}>{ messageResultImc }</Text>
+      <Text style={styles.numberImc}>{ resultImc }</Text>
     </View>
   )
 }
---
+++ src/styles/ResultImc.style.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  resultImc: {
+    flex: 1,
+    marginTop: 15,
+    paddingTop: 60,
+    borderRadius: 50,
+    alignItems: 'center',
+    width: '100%',
+  },
+  numberImc: {
+    fontSize: 48,
+    color: '#ff0043',
+    fontWeight: 'bold',
+  },
+  information: {
+    fontSize: 16,
+    color: '#ff0043',
+    // fontWeight: 'bold',
+  },
+  boxShareButton: {
+    width: '100%',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  share: {
+    backgroundColor: '#1877f2',
+    borderRadius: 50,
+    paddingBottom: 5,
+    paddingTop: 5,
+
+  },
+  sharedText: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    paddingHorizontal: 30
+  },
+})
+
+export default styles;
