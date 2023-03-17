76723b1125fada55dc77ed14ac2c92ae7ebdb796
elainecostadev <elainerscosta@hotmail.com>
2023-03-17 19:22:10 -0300

    inserindo styles no Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
+import styles from "../styles/Title.style";
 
 function Title() {
   return (
-    <View>
-      <Text>BEHEALTH!</Text>
+    <View style={styles.boxTitle}>
+      <Text style={styles.textTitle}>BEHEALTH!</Text>
     </View>
   )
 }
