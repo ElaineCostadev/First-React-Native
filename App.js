8b615d913ca16718e29ab771069dfb4923d8255e
elainecostadev <elainerscosta@hotmail.com>
2023-03-17 19:19:55 -0300

    :fix: retirando component Main e inserindo diretamente o Form

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StyleSheet, View } from 'react-native';
 import Title from './src/components/Title.js';
-import Main from './src/components/Main.js';
+import Form from './src/components/Form.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title />
-      <Main />
+      <Form />
     </View>
   );
 }
@@ -14,8 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E4E3D4',
+    paddingTop: 80,
   },
 });
